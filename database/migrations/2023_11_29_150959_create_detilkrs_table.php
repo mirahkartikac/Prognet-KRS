3adff5455f2673aa59387aae5ad753ee656254f8
Natania Setyawati <natania.nn64@gmail.com>
2023-12-15 23:39:52 +0800

    nothing important

--- database/migrations/2023_11_29_150959_create_detilkrs_table.php
+++ database/migrations/2023_11_29_150959_create_detilkrs_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->unsignedBigInteger('matakuliah_id');
             $table->string('nilai');
             $table->timestamps();
+
+            
         });
     }
 
